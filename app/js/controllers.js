51b87f73c98dbd3a4cdcbb4b734bb09d120b8f5b
John Jacobsen <john.jacobsen@gmail.com>
2020-05-26 15:53:19 -0400

    adding angular test

---
+++ app/js/controllers.js
@@ -0,0 +1,39 @@
+angular.module('fifaApp')
+
+	.controller('MainCtrl', ['UserService', function(UserService) {
+		var vm=this;
+		vm.userService = UserService;
+		UserService.session();
+	}])
+
+	.controller('TeamListCtrl', ['FifaService', function(FifaService) {
+		var vm=this;
+		vm.teams = [];
+		FifaService.getTeams.then(function(resp) {
+			vm.teams = resp.data;
+		});
+	}])
+
+	.controller('LoginCtrl', ['UserService', '$location', function(UserService, $location) {
+		var vm=this;
+		vm.user = {username: '', password: ''};
+		vm.login = function() {
+			UserService.login(self.user)
+				.then(function(success) {
+					$location.path('/team');
+				},function(error) {
+					vm.errorMessage = error.data.msg;
+				})
+		};
+	}])
+
+	.controller('TeamDetailsCtrl', ['$location', '$routeParam', 'FifaServicer', function($location, $routeParam, FifaService) {
+		var vm = this;
+		vm.team = {};
+		FifaService.getTeamDetails($routeParams.code)
+			.then(function(resp) {
+				vm.team = resp.data;
+			}, function(error) {
+				$location.path('/login');
+			});
+	}]);
